297bda8909044e600d08be8a37dcb1434a596ce5
micavoros <micavoros@gmail.com>
2023-03-09 23:15:31 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <ItemListContainer greeting="Productos no disponibles."/>
+    </>
+  );
+}
+export default App;
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function ItemListContainer ({greeting}) {
+    return(
+        <>
+            <h1>{greeting}</h1>
+        </>
+    )
+}
+export default ItemListContainer;
